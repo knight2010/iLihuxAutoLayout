c1d76d1b52c6fd8f1e0a15ee689b81fc36450e58
李辉 <hui.li@renren-inc.com>
2014-11-29 09:16:54 +0800

    创建swift版本的工程

---
+++ iLihuxAutoLayout/iLihuxAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iLihuxAutoLayout
+//
+//  Created by lihux on 14/11/29.
+//  Copyright (c) 2014年 lihux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
